850da858b52d3feb8f5d72336f75766d23366f6c
HEYROCKME <nedrum@gmail.com>
2024-08-21 15:34:33 +0200

    Added background and text assets

--- main.py
+++ main.py
@@ -17,20 +17,27 @@ pygame.display.set_caption('Runner')
 
 # Frame rate
 clock = pygame.time.Clock()
+test_font = pygame.font.Font('fonts/Monoton-Regular.ttf', 96)
 
-test_surface = pygame.Surface((100,200))
-test_surface.fill('gold')
+# test_surface = pygame.Surface((100,200))
+# test_surface.fill('gold')
+sky_surface = pygame.image.load('graphics/Sky.png')
+ground_surface = pygame.image.load('graphics/ground.png')
+text_surface = test_font.render('THE GAME', True, 'black')
 
 # Game Loop
 while True:
+    # Listening for events
     for event in pygame.event.get():
         # Quiting game
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
     
-    screen.blit(test_surface, (200,100))
-    # draw all elements 
+    # draw all elements in sequence 
+    screen.blit(sky_surface, (0,0))
+    screen.blit(ground_surface, (0, 300))
+    screen.blit(text_surface, (150, 20))
     # update everything
     pygame.display.update()
     
