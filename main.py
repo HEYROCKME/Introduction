91cff93969568165b699613ff4a688e8cc659771
HEYROCKME <nedrum@gmail.com>
2024-08-23 16:35:27 +0200

    game over logic

--- main.py
+++ main.py
@@ -31,7 +31,9 @@ test_font = pygame.font.Font('fonts/Monoton-Regular.ttf', 96)
 sky_surface = pygame.image.load('graphics/Sky.png').convert_alpha()
 ground_surface = pygame.image.load('graphics/ground.png').convert_alpha()
 score_surface = test_font.render('THE GAME', True, (64,64,64))
-score_rect = score_surface.get_rect(center = (400, 100))
+score_rect = score_surface.get_rect(center = (WIDTH / 2, 100))
+gameOver_surface = test_font.render('GAME OVER', True, "Red")
+gameOver_rect = gameOver_surface.get_rect(center = (WIDTH / 2, 200))
 
 snail_surface = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
 snail_rect = snail_surface.get_rect(midbottom=(700, 300))
@@ -48,13 +50,20 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if player_rect.collidepoint(event.pos) and player_rect.bottom >= 300:
-                player_gravity = - 20
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE and player_rect.bottom >= 300:
-                player_gravity = - 20
-        
+
+        # Controls
+        if game_active:    
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if player_rect.collidepoint(event.pos) and player_rect.bottom >= 300:
+                    player_gravity = - 20
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE and player_rect.bottom >= 300:
+                    player_gravity = - 20
+
+        else:       
+            if event.type == pygame.KEYDOWN and event.type == pygame.K_SPACE:
+                    game_active = True
+                    snail_rect.left = 800
 
     if game_active:
     # draw all elements in sequence 
@@ -79,7 +88,8 @@ while True:
 
         # Collision
         if snail_rect.colliderect(player_rect):
-            print('game over')
+            game_active = False
+            
 
         # if player_rect.colliderect(snail_rect):
         #    print('collsion')
@@ -91,6 +101,9 @@ while True:
         # keys = pygame.key.get_pressed()
         # if keys[pygame.K_SPACE]:
         #     print('jump')
+    else: 
+        screen.fill("Black")
+        screen.blit(gameOver_surface, gameOver_rect)
 
     # update everything
     pygame.display.update()
