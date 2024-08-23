3a194d88288e3b8afe81ae28e11b76c7556175cc
HEYROCKME <nedrum@gmail.com>
2024-08-23 12:06:10 +0200

    drawing examples

--- main.py
+++ main.py
@@ -9,7 +9,11 @@ pygame.init()
 # Set window size in pixels
 HEIGHT = 450
 WIDTH = 800
- 
+
+# Colors
+text_color = (64,64,64)
+boxcolor = '#c0e8ec' 
+
 # Screen
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
@@ -23,7 +27,8 @@ test_font = pygame.font.Font('fonts/Monoton-Regular.ttf', 96)
 # test_surface.fill('gold')
 sky_surface = pygame.image.load('graphics/Sky.png').convert_alpha()
 ground_surface = pygame.image.load('graphics/ground.png').convert_alpha()
-text_surface = test_font.render('THE GAME', True, 'black')
+score_surface = test_font.render('THE GAME', True, (64,64,64))
+score_rect = score_surface.get_rect(center = (400, 100))
 
 snail_surface = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
 snail_rect = snail_surface.get_rect(midbottom=(700, 300))
@@ -48,7 +53,11 @@ while True:
     # draw all elements in sequence 
     screen.blit(sky_surface, (0,0))
     screen.blit(ground_surface, (0, 300))
-    screen.blit(text_surface, (150, 20))
+    pygame.draw.rect(screen, boxcolor, score_rect)
+    pygame.draw.rect(screen, boxcolor, score_rect, 10)
+    # pygame.draw.line(screen,'Blue', (0,0), pygame.mouse.get_pos(), 3)
+    pygame.draw.ellipse(screen, 'Brown', pygame.Rect(50, 200,100,100))
+    screen.blit(score_surface, score_rect)
     # moving items
     snail_rect.x -= 5
     if snail_rect.right <= -1:
