fe087f74846bf29ad1b3fd91e6922a25ac7c5ce1
HEYROCKME <nedrum@gmail.com>
2024-08-23 10:09:04 +0200

    added npc and main char

--- main.py
+++ main.py
@@ -21,9 +21,16 @@ test_font = pygame.font.Font('fonts/Monoton-Regular.ttf', 96)
 
 # test_surface = pygame.Surface((100,200))
 # test_surface.fill('gold')
-sky_surface = pygame.image.load('graphics/Sky.png')
-ground_surface = pygame.image.load('graphics/ground.png')
+sky_surface = pygame.image.load('graphics/Sky.png').convert_alpha()
+ground_surface = pygame.image.load('graphics/ground.png').convert_alpha()
 text_surface = test_font.render('THE GAME', True, 'black')
+
+snail_surface = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
+print(snail_surface)
+snail_x_position = 600
+
+player_surface = pygame.image.load('graphics/player/player_walk_1.png').convert_alpha()
+player_rect = player_surface.get_rect(topleft= (100, 265))
 
 # Game Loop
 while True:
@@ -38,6 +45,13 @@ while True:
     screen.blit(sky_surface, (0,0))
     screen.blit(ground_surface, (0, 300))
     screen.blit(text_surface, (150, 20))
+    # moving items
+    snail_x_position -= 5
+    if snail_x_position <= -72:
+        snail_x_position = 800
+    screen.blit(snail_surface, (snail_x_position, 265))
+    screen.blit(player_surface, player_rect)
+
     # update everything
     pygame.display.update()
     
